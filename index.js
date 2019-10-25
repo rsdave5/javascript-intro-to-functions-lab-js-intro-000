17b4d3870e7eeda4f264503aa30f355ef9cda000
Reetu Dave <rsdave5@gmail.com>
2019-10-25 17:49:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-var uppercase = "HELLO!"
-var lowercase = "hello!"
+var uppercase = "HELLO"
+var lowercase = "hello"
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === lowercase) {
