ce212444b2c5a825b1e79a8f2f37437a964e8e21
Reetu Dave <rsdave5@gmail.com>
2019-10-25 17:56:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ var uppercase = "HELLO"
 var lowercase = "hello"
 
 function sayHiToGrandma(string) {
-  if (sayHiToGrandma(string) === lowercase) {
+  if (string === lowercase) {
     return "I can't hear you!"
-  } else if (sayHiToGrandma(string) === uppercase) {
+  } else if (string === uppercase) {
     return "YES INDEED!"
-  } else if (sayHiToGrandma(string) === "I love you, Grandma.") {
+  } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
